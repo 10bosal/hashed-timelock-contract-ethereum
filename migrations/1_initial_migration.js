3c7c88727cac6c42e683e11cb4ee4d7ad3536807
JANG Wooyeong <vidyo@hanmail.net>
2021-08-29 13:44:02 +0900

    스마트컨트랙트 배포용 계정 지정 (Ganache)

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 var Migrations = artifacts.require("./Migrations.sol")
+const deployerAccount = "0xCA6d0A7c597fD54f6FF6Faf5014ff7b3Fd318d20"
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations)
+  deployer.deploy(Migrations, { from: deployerAccount })
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,10 @@
 const HashedTimelock = artifacts.require('./HashedTimelock.sol')
 const HashedTimelockERC20 = artifacts.require('./HashedTimelockERC20.sol')
 const HashedTimelockERC721 = artifacts.require('./HashedTimelockERC721.sol')
+const deployerAccount = "0xCA6d0A7c597fD54f6FF6Faf5014ff7b3Fd318d20"
 
 module.exports = function (deployer) {
-  deployer.deploy(HashedTimelock)
-  deployer.deploy(HashedTimelockERC20)
-  deployer.deploy(HashedTimelockERC721)
+  deployer.deploy(HashedTimelock, { from: deployerAccount })
+  deployer.deploy(HashedTimelockERC20, { from: deployerAccount })
+  deployer.deploy(HashedTimelockERC721, { from: deployerAccount })
 }
